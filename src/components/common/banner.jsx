902838f9aaabd2bb2eac847eefed668a4c39848b
Bilguutei <85947974+balguti@users.noreply.github.com>
2024-03-20 16:55:18 +0800

    create banner

---
+++ src/components/common/banner.jsx
@@ -0,0 +1,16 @@
+import ImageContainer from "./ImageContainer";
+
+export default function Banner({ title, description }) {
+  return (
+    <div className="relative w-full h-64 ">
+      <ImageContainer src="/background/bg.png" />
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="container text-center text-white">
+          <h1 className="text-3xl uppercase lg:text-5xl">{title}</h1>
+          <br />
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
