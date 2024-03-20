f7540c3634abb16cf25dc4139aec829680889717
Bilguutei <85947974+balguti@users.noreply.github.com>
2024-03-20 23:41:50 +0800

    update

---
+++ src/components/common/MemberPopup.jsx
@@ -0,0 +1,78 @@
+import { OnClickOutside } from "@/lib/utils/on-click-outside";
+import ImageContainer from "./ImageContainer";
+import { useRef } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+export default function MemberPopup({
+  member,
+  onClose,
+  onClickPrev,
+  onClickNext,
+}) {
+  const ref = useRef(null);
+
+  OnClickOutside(ref, onClose, member);
+  return (
+    <AnimatePresence>
+      {member && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3, type: "spring" }}
+          className="fixed inset-0 z-50 flex items-center justify-center p-4 backdrop-brightness-75"
+        >
+          <motion.div
+            ref={ref}
+            initial={{ scale: 0.75 }}
+            animate={{ scale: 1 }}
+            exit={{ scale: 0.75 }}
+            transition={{ duration: 0.3, type: "spring" }}
+            className="max-h-[calc(100svh-2rem)] lg:max-h-[calc(100svh-4rem)] max-w-[1062px] flex flex-col sm:flex-row p-4 lg:p-8 w-full bg-white rounded-lg gap-4 lg:gap-8 relative overflow-auto"
+          >
+            <div className="absolute flex items-center justify-center w-8 h-8 rounded-full cursor-pointer top-4 right-4 hover:bg-base-50">
+              <i className="text-3xl ri-close-line" onClick={onClose}></i>
+            </div>
+            <div className="w-full sm:w-[296px] flex items-center flex-shrink-0 mt-12 sm:mt-0">
+              <div className="w-full aspect-square border border-[#4E4B6633] relative bg-slate-100 rounded-full overflow-hidden">
+                <ImageContainer src={member.image} />
+              </div>
+            </div>
+            <div className="flex-1">
+              <h4 className="mb-1 text-xl font-semibold uppercase lg:text-3xl">
+                {member.name}
+              </h4>
+              <p className="mb-4 text-lg text-blue lg:mb-16">{member.role}</p>
+              <p className="mb-2">{member.position}</p>
+              <p className="mb-2">{member.proffesion}</p>
+              <p className="mb-4 lg:mb-16">{member.education}</p>
+              <div className="w-full h-[1px] bg-[#4E4B6633] mb-4"></div>
+              <div className="flex">
+                <div className="flex-1">
+                  <p className="text-lg uppercase ">
+                    phone number - {member.phone}
+                  </p>
+                  <p className="text-lg uppercase ">email - {member.email}</p>
+                </div>
+                <div className="flex items-end flex-shrink-0 gap-2">
+                  <div
+                    onClick={onClickPrev}
+                    className="flex items-center justify-center w-12 h-12 text-white rounded cursor-pointer bg-blue"
+                  >
+                    <i className="text-lg ri-arrow-left-line"></i>
+                  </div>
+                  <div
+                    onClick={onClickNext}
+                    className="flex items-center justify-center w-12 h-12 text-white rounded cursor-pointer bg-blue"
+                  >
+                    <i className="text-lg ri-arrow-right-line"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
--- src/pages/staff.jsx
+++ src/pages/staff.jsx
@@ -2,61 +2,58 @@ import React from "react";
 import ImageContainer from "@/components/common/ImageContainer";
 import Image from "next/image";
 import Banner from "@/components/common/banner";
-
 import { useState } from "react";
+import MemberPopup from "@/components/common/MemberPopup";
+
 export default function StaffPage() {
+  const [currentMember, setCurrentMember] = useState(null);
+
   const roleList = [
     {
       image: "/image/staff-profile.png",
-      name: "lee song woong",
+      name: "lee song woong 1",
       role: "CEO",
       position: "Head of corporate research institute",
       proffesion: "Industrial engineer",
       education: "Office Information Equipment Application Technician Level 2",
-      phone: "phone number - 010-4588-2794 ",
-      email: "email - incom2794@naver.com",
+      phone: "010-4588-2794 ",
+      email: "incom2794@naver.com",
     },
     {
       image: "/image/staff-profile.png",
-      name: "lee song woong",
+      name: "lee song woong 2",
       role: "CEO",
       position: "Head of corporate research institute",
       proffesion: "Industrial engineer",
       education: "Office Information Equipment Application Technician Level 2",
-      phone: "phone number - 010-4588-2794 ",
-      email: "email - incom2794@naver.com",
+      phone: "010-4588-2794 ",
+      email: "incom2794@naver.com",
     },
     {
       image: "/image/staff-profile.png",
-      name: "lee song woong",
+      name: "lee song woong 3",
       role: "CEO",
       position: "Head of corporate research institute",
       proffesion: "Industrial engineer",
       education: "Office Information Equipment Application Technician Level 2",
-      phone: "phone number - 010-4588-2794 ",
-      email: "email - incom2794@naver.com",
+      phone: "010-4588-2794 ",
+      email: "incom2794@naver.com",
     },
     {
       image: "/image/staff-profile.png",
-      name: "lee song woong",
+      name: "lee song woong 4",
       role: "CEO",
       position: "Head of corporate research institute",
       proffesion: "Industrial engineer",
       education: "Office Information Equipment Application Technician Level 2",
-      phone: "phone number - 010-4588-2794 ",
-      email: "email - incom2794@naver.com",
-    },
-    {
-      image: "/image/staff-profile.png",
-      name: "lee song woong",
-      role: "CEO",
-      position: "Head of corporate research institute",
-      proffesion: "Industrial engineer",
-      education: "Office Information Equipment Application Technician Level 2",
-      phone: "phone number - 010-4588-2794 ",
-      email: "email - incom2794@naver.com",
+      phone: "010-4588-2794 ",
+      email: "incom2794@naver.com",
     },
   ];
+
+  const selectedMember =
+    currentMember !== null ? roleList[currentMember] : null;
+
   return (
     <>
       <div className="relative lg:justify-center">
@@ -69,8 +66,16 @@ export default function StaffPage() {
             <div className="grid w-full grid-cols-2 gap-12 lg:grid-cols-4 ">
               {roleList.map(({ image, name, role }, index) => (
                 <div key={index} className="w-full">
-                  <div className="relative flex items-center justify-center overflow-hidden bg-gray-300 border rounded-full border-gray aspect-square">
+                  <div
+                    className="relative z-0 flex items-center justify-center overflow-hidden bg-gray-300 border rounded-full cursor-pointer border-gray aspect-square"
+                    onClick={() => setCurrentMember(index)}
+                  >
                     <ImageContainer src={image} />
+                    <div className="absolute inset-0 z-10 flex items-center justify-center transition opacity-0 backdrop-brightness-50 hover:opacity-100">
+                      <p className="font-light text-white uppercase">
+                        See more
+                      </p>
+                    </div>
                   </div>
                   <br />
                   <div>
@@ -87,6 +92,20 @@ export default function StaffPage() {
           </div>
         </div>
       </div>
+      <MemberPopup
+        member={selectedMember}
+        onClose={() => setCurrentMember(null)}
+        onClickPrev={() => {
+          if (currentMember > 0) {
+            setCurrentMember(currentMember - 1);
+          }
+        }}
+        onClickNext={() => {
+          if (currentMember < roleList.length - 1) {
+            setCurrentMember(currentMember + 1);
+          }
+        }}
+      />
     </>
   );
 }
