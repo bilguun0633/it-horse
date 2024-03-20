7d744e4b1b4a7547f0a3b8807631aa90ce95702a
Bilguutei <85947974+balguti@users.noreply.github.com>
2024-03-20 18:11:44 +0800

    update header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-import { OnClickOutside } from "@/lib/utils/on-click-outside";
-import { useState, useRef } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import Hamburger from "hamburger-react";
 import { useRouter } from "next/router";
 import Flag from "react-flagkit";
@@ -10,8 +9,6 @@ import useTranslation from "next-translate/useTranslation";
 
 export default function Header() {
   // const { t } = useTranslation("main");
-
-  const [show, setShow] = useState(false);
 
   const router = useRouter();
 
@@ -24,6 +21,24 @@ export default function Header() {
   const { pathname } = useRouter();
 
   const isHome = pathname === "/";
+  const [isHover, setIsHover] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 64) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const list = [
     {
@@ -108,174 +123,59 @@ export default function Header() {
 
   return (
     <div
-      className={`top-0 inset-x-0 z-50 h-fit transitioned ${
-        isHome ? "fixed text-white " : "bg-white text-black sticky"
+      className={`fixed inset-x-0 top-0 z-50 h-fit transition ${
+        !isHome || isHover || isScrolled
+          ? "bg-white"
+          : "text-white bg-transparent"
       }`}
     >
-      <div className="relative container lg:flex lg:justify-end">
-        <Link href="/">
-          <div className="absolute flex items-center h-24 left-4">
-            <Image
-              src={isHome ? `/image/logo.png` : "/image/logo-dark.png"}
-              alt=""
-              width={187}
-              height={28}
-            />
+      <div className="container">
+        <div className="relative flex items-center justify-center w-full">
+          <div className="absolute top-0 left-0 flex items-center justify-center h-16 lg:h-24">
+            <Link href="/">
+              <Image
+                src={`/icons/it-horse-${
+                  !isHome || isHover || isScrolled ? "blue" : "white"
+                }.png`}
+                alt="logo"
+                width={50}
+                height={50}
+                layout="fixed"
+              />
+            </Link>
           </div>
-        </Link>
-        <div className="h-24 hover:h-fit transition-all overflow-hidden gap-4 2xl:gap-14 lg:flex ">
-          {list.map(({ url, name, dropdown }, index) => (
-            <div className="" key={index}>
-              <Link href={url}>
-                <div className="flex items-center h-24 text-sm cursor-pointer xl:text-base transitioned hover:text-primary">
-                  {name}
-                </div>
-              </Link>
-
-              <div className="flex flex-col gap-4">
-                {dropdown?.map(({ name, url }, index) => (
-                  <Link href={url} key={index}>
-                    <div className="flex items-center text-sm cursor-pointer xl:text-base transitioned hover:text-primary">
-                      {name}
-                    </div>
-                  </Link>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-
-        <div className="items-center flex hover:text-primary">KR | MN | EN</div>
-      </div>
-    </div>
-  );
-}
-
-function Dropdown({ name, dropdown, url }) {
-  const router = useRouter();
-
-  const [show, setShow] = useState(false);
-
-  const ref = useRef();
-
-  OnClickOutside(ref, () => setShow(false), show);
-  return (
-    <div
-      className="relative h-full cursor-pointer text-base-600 hover:text-primary transitioned"
-      onMouseOver={() => setShow(true)}
-      ref={ref}
-    >
-      <div
-        className="flex items-center h-full gap-0.5 text-sm xl:text-base"
-        onClick={() => {
-          if (url) {
-            router.push(url);
-          }
-        }}
-      >
-        {name}
-        <i className="mt-0.5 text-xl ri-arrow-down-s-line"></i>
-      </div>
-      <AnimatePresence>
-        {show && (
           <motion.div
-            initial={{ opacity: 0, y: -3 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -3 }}
-            transition={{ duration: 0.3, type: "spring" }}
-            onMouseLeave={() => setShow(false)}
-            className="absolute flex flex-col gap-1 p-6 right-0 xl:left-0 rounded shadow-lg w-72 top-[100px] bg-white z-50"
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
+            transition={{ duration: 0.3 }}
+            animate={{ height: isHover ? "auto" : "96px" }}
+            className="hidden gap-12 overflow-hidden lg:flex"
           >
-            {dropdown.map(({ name, url }, index) => (
-              <Link href={url} key={index}>
-                <div className="w-full p-3 rounded cursor-pointer text-base-300 hover:text-primary transitioned hover:bg-black/5">
+            {list.map(({ name, dropdown }, index) => (
+              <div key={index}>
+                <p className="flex items-center justify-center h-24 font-bold transition cursor-default">
                   {name}
-                </div>
-              </Link>
-            ))}
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
-  );
-}
-
-function MobileDropdown({ show, list, onClose, handleLocale, router }) {
-  const [dropdownIndex, setDropdownIndex] = useState(null);
-
-  function handleClick(index) {
-    if (dropdownIndex === index) {
-      setDropdownIndex(null);
-    } else {
-      setDropdownIndex(index);
-    }
-  }
-
-  return (
-    <AnimatePresence>
-      {show && (
-        <motion.div
-          initial={{ opacity: 0, height: 0 }}
-          animate={{ opacity: 1, height: "auto" }}
-          exit={{ opacity: 0, height: 0 }}
-          transition={{ duration: 0.3 }}
-          className="absolute w-full overflow-hidden bg-white border-b top-16 border-base-50"
-        >
-          <div className="flex flex-col items-center gap-4 p-4">
-            {list.map(({ dropdown, name, url }, index) => {
-              if (dropdown) {
-                return (
-                  <div key={index}>
-                    <h4
-                      className="flex items-center h-full gap-2 text-lg cursor-pointer lg:text-xl text-base-600 active:scale-75 transitioned"
-                      onClick={() => handleClick(index)}
+                </p>
+                <motion.div
+                  animate={{ opacity: isHover ? 1 : 0 }}
+                  className="flex flex-col items-center gap-4 pb-12 text-sm text-[#586772]"
+                >
+                  {dropdown.map(({ name, url }, index) => (
+                    <Link
+                      href={url}
+                      key={index}
+                      className="transition hover:text-blue"
+                      onClick={() => setIsHover(false)}
                     >
                       {name}
-                      <i
-                        className={`mt-0.5 text-2xl ri-arrow-down-s-line transitioned ${
-                          dropdownIndex === index ? "rotate-180" : ""
-                        }`}
-                      ></i>
-                    </h4>
-                    <AnimatePresence>
-                      {dropdownIndex === index && (
-                        <motion.div
-                          initial={{ opacity: 0, height: 0 }}
-                          animate={{ opacity: 1, height: "auto" }}
-                          exit={{ opacity: 0, height: 0 }}
-                          transition={{ duration: 0.3 }}
-                          className="overflow-hidden"
-                        >
-                          <div className="flex flex-col items-center gap-4 pt-4">
-                            {dropdown.map(({ name, url }, index) => (
-                              <Link href={url} key={index} onClick={onClose}>
-                                <div className="flex items-center h-full cursor-pointer text-base-400 active:text-primary transitioned">
-                                  {name}
-                                </div>
-                              </Link>
-                            ))}
-                          </div>
-                        </motion.div>
-                      )}
-                    </AnimatePresence>
-                  </div>
-                );
-              } else {
-                return (
-                  <Link href={url} key={index} onClick={onClose}>
-                    <div className="flex items-center h-full text-lg cursor-pointer lg:text-xl text-base-600 active:scale-75 transitioned">
-                      {name}
-                    </div>
-                  </Link>
-                );
-              }
-            })}
-            {/* <button onClick={handleLocale}>
-              <Flag country={router.locale === "en" ? "MN" : "US"} />
-            </button> */}
-          </div>
-        </motion.div>
-      )}
-    </AnimatePresence>
+                    </Link>
+                  ))}
+                </motion.div>
+              </div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/layout/Provider.jsx
+++ src/components/layout/Provider.jsx
@@ -1,11 +1,20 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import { useRouter } from "next/router";
 
 export default function Provider({ children }) {
+  const { pathname } = useRouter();
+
+  const isHome = pathname === "/";
+
   return (
     <div>
       <Header />
-      <main className="min-h-[calc(100svh-64px)] lg:min-h-[calc(100svh-96px)] flex flex-col">
+      <main
+        className={`min-h-[calc(100svh-64px)] lg:min-h-[calc(100svh-96px)] flex flex-col ${
+          !isHome ? "mt-16 lg:mt-24" : ""
+        }`}
+      >
         {children}
       </main>
       <Footer />
