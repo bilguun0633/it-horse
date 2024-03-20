c0b0aefcbee9c3885df9b5b23efb9978d8e8c809
Bilguutei <85947974+balguti@users.noreply.github.com>
2024-03-20 19:59:08 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "43.231.113.143",
+        port: "3003",
+      },
+      {
+        protocol: "http",
+        hostname: "ithorse.mn",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
